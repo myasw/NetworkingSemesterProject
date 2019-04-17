55ba3743ac64db84865451dc9e4e6b433f3704e5
myasw <mryasby@gmail.com>
2019-04-17 11:31:33 -0400

    Added a frame to choose the GUI version

--- src/ClientMobile.java
+++ src/ClientMobile.java
@@ -7,6 +7,7 @@ import java.awt.event.ActionListener;
 
  	private JFrame frame;
  	private JFrame frame2;
+ 	private JFrame f;
 	private JTextField userTextField;
 	private JTextField textField_1;
 	private JLabel userLabel;
@@ -161,12 +162,41 @@ import java.awt.event.ActionListener;
 		newAcctBtn = new JButton("New Account");
 		newAcctBtn.setFont(new Font("Times New Roman", Font.BOLD, 20));
 		newAcctBtn.setBounds(281, 242, 218, 38);
-		newAcctBtn.addActionListener(new ActionListener() { 
-			public void actionPerformed(ActionEvent e) { 
-				  frame2.dispose();
-				  frame.setVisible(true);
+		
+		connectBtn.addActionListener(new ActionListener() { 
+			public void actionPerformed(ActionEvent e) {
+				frame2.dispose();
+				f = new JFrame("Choose a version");
+				f.getContentPane().setFont(new Font("Times New Roman", Font.PLAIN, 14));
+				f.setBounds(0, 0, 800, 800);
+				f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				f.getContentPane().setLayout(null);
+				f.setVisible(true);
+				
+				JLabel iDFText2 = new JLabel("Choose a version");
+				iDFText2.setHorizontalAlignment(SwingConstants.CENTER);
+				iDFText2.setFont(new Font("Times New Roman", Font.BOLD, 30));
+				iDFText2.setBounds(148, 152, 501, 58);
+				f.getContentPane().add(iDFText2);
+				
+				JButton mobileBtn = new JButton("Mobile");
+				mobileBtn.setFont(new Font("Times New Roman", Font.BOLD, 20));
+				mobileBtn.setBounds(90, 385, 218, 38);
+				f.getContentPane().add(mobileBtn);
+				
+				JButton atmBtn = new JButton("ATM");
+				atmBtn.setFont(new Font("Times New Roman", Font.BOLD, 20));
+				atmBtn.setBounds(457, 385, 218, 38);
+				f.getContentPane().add(atmBtn);
+				mobileBtn.addActionListener(new ActionListener() { 
+					public void actionPerformed(ActionEvent e) { 
+						f.dispose();
+						frame.setVisible(true);
+					}
+				});
 			} 
 		});
+		
 		frame2.getContentPane().add(newAcctBtn);
 
 
