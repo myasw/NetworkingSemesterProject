e1a14574e4227a048cdbe6fc19a8391cf0dfe599
myasw <mryasby@gmail.com>
2019-04-17 12:53:40 -0400

    Made ATM GUI frame

--- src/ClientMobile.java
+++ src/ClientMobile.java
@@ -7,20 +7,27 @@ import java.awt.event.ActionListener;
 
  	private JFrame frame;
  	private JFrame frame2;
+ 	private JFrame frame3;
  	private JFrame f;
 	private JTextField userTextField;
-	private JTextField textField_1;
+	private JTextField pswdTextField;
 	private JLabel userLabel;
-	private JLabel pswdTextField;
+	private JLabel pswdLabel;
 	private JButton disconnectBtn;
 	private JTextField balanceField;
+	private JTextField balanceField2;
 	private JTextField scoreField;
 	private JButton freezeBtn;
 	private JTextField transferField;
 	private JButton balanceBtn;
+	private JButton balanceBtn2;
 	private JButton scoreBtn;
 	private JButton transferBtn;
 	private JButton newAcctBtn;
+	private JTextField depositField;
+	private JTextField withdrawField;
+	private JButton depositBtn;
+	private JButton withdrawBtn;
 
  	/**
 	 * Launch the application.
@@ -32,6 +39,7 @@ import java.awt.event.ActionListener;
 					ClientMobile window = new ClientMobile();
 					window.frame.setVisible(false);
 					window.frame2.setVisible(true);
+					window.frame3.setVisible(false);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -74,11 +82,11 @@ import java.awt.event.ActionListener;
 		frame2.getContentPane().add(userTextField);
 		userTextField.setColumns(10);
 
- 		textField_1 = new JTextField();
-		textField_1.setFont(new Font("Times New Roman", Font.PLAIN, 20));
-		textField_1.setBounds(457, 42, 218, 33);
-		frame2.getContentPane().add(textField_1);
-		textField_1.setColumns(10);
+ 		pswdTextField = new JTextField();
+		pswdTextField.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+		pswdTextField.setBounds(457, 42, 218, 33);
+		frame2.getContentPane().add(pswdTextField);
+		pswdTextField.setColumns(10);
 
  		userLabel = new JLabel("Username");
 		userLabel.setHorizontalAlignment(SwingConstants.CENTER);
@@ -86,11 +94,11 @@ import java.awt.event.ActionListener;
 		userLabel.setBounds(90, 15, 218, 15);
 		frame2.getContentPane().add(userLabel);
 
- 		pswdTextField = new JLabel("Password");
-		pswdTextField.setHorizontalAlignment(SwingConstants.CENTER);
-		pswdTextField.setFont(new Font("Times New Roman", Font.BOLD, 20));
-		pswdTextField.setBounds(457, 12, 218, 21);
-		frame2.getContentPane().add(pswdTextField);
+ 		pswdLabel = new JLabel("Password");
+		pswdLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		pswdLabel.setFont(new Font("Times New Roman", Font.BOLD, 20));
+		pswdLabel.setBounds(457, 12, 218, 21);
+		frame2.getContentPane().add(pswdLabel);
 
  		JButton connectBtn = new JButton("Connect");
 		connectBtn.setFont(new Font("Times New Roman", Font.BOLD, 18));
@@ -144,7 +152,7 @@ import java.awt.event.ActionListener;
 		radioPanel.setBounds(105, 526, 186, 99);
 		frame.getContentPane().add(radioPanel);
 
-
+		
 
  		ButtonGroup group = new ButtonGroup();
 		JRadioButton checkingBtn = new JRadioButton("Checking");
@@ -188,6 +196,14 @@ import java.awt.event.ActionListener;
 				atmBtn.setFont(new Font("Times New Roman", Font.BOLD, 20));
 				atmBtn.setBounds(457, 385, 218, 38);
 				f.getContentPane().add(atmBtn);
+				
+				atmBtn.addActionListener(new ActionListener() { 
+					public void actionPerformed(ActionEvent e) { 
+						f.dispose();
+						frame3.setVisible(true);
+					}
+				});
+				
 				mobileBtn.addActionListener(new ActionListener() { 
 					public void actionPerformed(ActionEvent e) { 
 						f.dispose();
@@ -199,7 +215,71 @@ import java.awt.event.ActionListener;
 		
 		frame2.getContentPane().add(newAcctBtn);
 
+		frame3 = new JFrame("Mobile Banking GUI");
+		frame3.getContentPane().setFont(new Font("Times New Roman", Font.PLAIN, 14));
+		frame3.setBounds(0, 0, 800, 800);
+		frame3.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame3.getContentPane().setLayout(null);
 
+		JLabel iDFText3 = new JLabel("Hello...\nWhat would you like to do?");
+		iDFText3.setHorizontalAlignment(SwingConstants.CENTER);
+		iDFText3.setFont(new Font("Times New Roman", Font.BOLD, 30));
+		iDFText3.setBounds(148, 152, 501, 58);
+		frame3.getContentPane().add(iDFText3);
 
+ 		disconnectBtn = new JButton("Disconnect");
+		disconnectBtn.setFont(new Font("Times New Roman", Font.BOLD, 18));
+		disconnectBtn.setBounds(457, 100, 218, 42);
+		frame3.getContentPane().add(disconnectBtn);
+
+ 		balanceField2 = new JTextField();
+		balanceField2.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+		balanceField2.setBounds(90, 334, 218, 39);
+		frame3.getContentPane().add(balanceField2);
+		balanceField2.setColumns(10);
+
+ 		depositField = new JTextField();
+		depositField.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+		depositField.setBounds(457, 334, 218, 39);
+		frame3.getContentPane().add(depositField);
+		depositField.setColumns(10);
+
+ 		withdrawField = new JTextField();
+		withdrawField.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+		withdrawField.setBounds(457, 459, 218, 38);
+		frame3.getContentPane().add(withdrawField);
+		withdrawField.setColumns(10);
+
+ 		balanceBtn2 = new JButton("Balance");
+		balanceBtn2.setFont(new Font("Times New Roman", Font.BOLD, 20));
+		balanceBtn2.setBounds(90, 385, 218, 38);
+		frame3.getContentPane().add(balanceBtn2);
+
+ 		depositBtn = new JButton("Deposit");
+		depositBtn.setFont(new Font("Times New Roman", Font.BOLD, 20));
+		depositBtn.setBounds(457, 385, 218, 38);
+		frame3.getContentPane().add(depositBtn);
+
+ 		withdrawBtn = new JButton("Withdraw");
+		withdrawBtn.setFont(new Font("Times New Roman", Font.BOLD, 20));
+		withdrawBtn.setBounds(457, 509, 218, 48);
+		frame3.getContentPane().add(withdrawBtn);
+
+		JPanel radioPanel2 = new JPanel();
+		radioPanel2.setBounds(105, 526, 186, 99);
+		frame3.getContentPane().add(radioPanel2);
+		
+		ButtonGroup group2 = new ButtonGroup();
+		JRadioButton checkingBtn2 = new JRadioButton("Checking");
+		checkingBtn2.setHorizontalAlignment(SwingConstants.CENTER);
+		checkingBtn2.setFont(new Font("Times New Roman", Font.BOLD, 20));
+		JRadioButton savingsBtn2 = new JRadioButton("Savings");
+		savingsBtn2.setFont(new Font("Times New Roman", Font.BOLD, 20));
+		savingsBtn2.setHorizontalAlignment(SwingConstants.CENTER);
+		group2.add(checkingBtn2);
+		group2.add(savingsBtn2);
+
+ 		radioPanel2.add(checkingBtn2);
+		radioPanel2.add(savingsBtn2);
  	}
 }
